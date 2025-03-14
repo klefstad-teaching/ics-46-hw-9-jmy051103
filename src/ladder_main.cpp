6f087dd5dfa6dd28c3cc2dcee3222b01d9d0b701
jmy051103 <jaeminy@uci.edu>
2025-03-14 05:24:21 +0000

    .

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     set<string> word_list;
-    string file_name = "../words.txt";
+    string file_name = "words.txt";
     load_words(word_list, file_name);
 
     string start, end;
