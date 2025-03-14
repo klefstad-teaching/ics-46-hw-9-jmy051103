f9e92ec48f7e4bd44a60d75efe24cfccddd518dc
jmy051103 <jaeminy@uci.edu>
2025-03-14 05:45:54 +0000

    .

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -7,18 +7,36 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const string& str1, const string& str2, int d) {
-    int len1 = str1.size(), len2 = str2.size();
-    if (abs(len1 - len2) > d) return false;
+    int len1 = str1.size();
+    int len2 = str2.size();
+    if (abs(len1 - len2) > d) {
+        return false;
+    }
 
-    int edit = 0, i = 0, j = 0;
+    int edit = 0;
+    int i = 0;
+    int j = 0;
     while (i < len1 && j < len2) {
         if (str1[i] != str2[j]) {
             edit++;
-            if (edit > d) return false;
-            if (len1 > len2) i++;      
-            else if (len1 < len2) j++; 
-            else { i++; j++; }         
-        } else { i++; j++; } 
+            if (edit > d) {
+                return false;
+            }
+            if (len1 > len2) {
+                i++;
+            }     
+            else if (len1 < len2) {
+                j++;
+            }
+            else { 
+                i++; 
+                j++; 
+            }         
+        } 
+        else { 
+            i++; 
+            j++; 
+        } 
     }
     edit += (len1 - i) + (len2 - j);
     return edit <= d;
