014cd3a5868552ae15a200b183e70e1947efa80c
jmy051103 <jaeminy@uci.edu>
2025-03-14 05:07:03 +0000

    .

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -55,6 +55,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
+        cout << "\nTotal cost is " << total << endl;
         return;
     }
     for (size_t i = 0; i < path.size(); ++i) {
